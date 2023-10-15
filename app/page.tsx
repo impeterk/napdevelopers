9468271638c856a86d6fce6853310dbd6e3c98f9
peter <peterkudelas@gmail.com>
2023-10-15 22:26:22 +0200

    footer with emogies

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
 
       <div className="grid text-center lg:max-w-5xl lg:w-full pb-8">
         <p className="text-slate-950 dark:invert">
-          {`© ${new Date().getFullYear()} | NaP Developers`}
+          {`© ${new Date().getFullYear()} | NaP 💤 Developers 💻`}
         </p>
       </div>
     </main>
