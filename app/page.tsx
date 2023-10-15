35e8e163bcc5f24f6089b041fe7127d9110a23bb
peter <peterkudelas@gmail.com>
2023-10-15 22:17:31 +0200

    blur upgrades

--- app/page.tsx
+++ app/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
         </p>
       </div>
 
-      <div className="px-4 relative flex place-items-center before:absolute before:h-[300px] before:w-[580px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-inherit before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[340px] md:after:translate-x-[15%] after:bg-gradient-conic after:from-sky-500 after:via-blue-600 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-inherit before:dark:to-sky-700 before:dark:opacity-10 after:dark:from-sku-500 after:dark:via-blue-700 after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+      <div className="px-4 relative flex place-items-center before:absolute before:h-[100px] before:w-[380px] before:translate-x-1/3 before:rounded-full before:bg-gradient-radial before:from-transparent before:to-sky-700/10 before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[200px] md:after:h-[300px] after:w-[375px] max-sm:after:-translate-x-[10%] after:bg-gradient-conic after:from-cyan-500 after:to-blue-700 after:blur-3xl lg:after:blur-[100px] after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-sky-700/10 before:dark:opacity-10 after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <div className="flex flex-col gap-3 items-center">
-          <p className="relative   dark:text-slate-300 text-slate-700 max-[375px]:text-2xl text-3xl md:text-4xl font-black tracking-widest">
+          <p className="relative   dark:text-slate-300 text-slate-800 max-[375px]:text-2xl text-3xl md:text-4xl font-black tracking-widest">
             {`<NaP Developers />`}
           </p>
           <p className="relative text-center   dark:text-slate-300 text-slate-700  max-[375px]:text-sm md:text-xl font-bold tracking-wide">
